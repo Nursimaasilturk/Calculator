0bd8ff256ae906a2d0fd3faec473152f3f92f363
Nursima Asilturk <nursima.asilturk99@gmail.com>
2022-08-15 03:41:03 +0300

    all problems are fixed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const operation = document.getElementById("operation");
-var historyText = document.getElementById("history-text");
-var firstCalc = document.getElementById("first-eva");
-var secondCalc = document.getElementById("second-eva");
-var thirdCalc = document.getElementById("third-eva");
-var calcArray = new Array();
-var resultArray = new Array();
+const historyText = document.getElementById("history-text");
+const firstCalc = document.getElementById("first-eva");
+const secondCalc = document.getElementById("second-eva");
+const thirdCalc = document.getElementById("third-eva");
+const calcArray = new Array();
+const resultArray = new Array();
 $(document).ready(function () {
   // ---> Dark-Light Mode
   $(".screen--modes__light-dark").click(function () {
@@ -69,11 +69,91 @@ $(document).ready(function () {
     operation.value = null;
   });
   // ---> Change the sign of number
-  $("#sign").click(function () {});
-  // ---> Percentage Calculate
-  $("#perc").click(function () {});
-  // ---> Invert values of input to numbers and calculate
-  $("#equal").click(function () {
+  $("#sign").click(function () {
+    operation.value = -1 * basicCalculate(operation.value);
+  });
+  // ---> basic oparation function
+  const basicCalculate = function (e) {
+    var sign = []; // --> For signs in value
+    var calculatedValue = e;
+    var withoutSpace = calculatedValue.replace(/\s/g, ""); // --->clean all spaces
+    //--> assigning the signs inside the operation to the sign array
+    for (i = 0; i < withoutSpace.length - 1; i++) {
+      if (withoutSpace[i] === "/") {
+        sign.push(withoutSpace[i]);
+      } else if (withoutSpace[i] === "*") {
+        sign.push(withoutSpace[i]);
+      } else if (withoutSpace[i] === "-") {
+        sign.push(withoutSpace[i]);
+      } else if (withoutSpace[i] === "+") {
+        sign.push(withoutSpace[i]);
+      } else if (withoutSpace[i] === "%") {
+        sign.push(withoutSpace[i]);
+      }
+    }
+    // --> assigning all numbers inside the operation to the array
+    const numbers = withoutSpace
+      .replaceAll("*", "$")
+      .replaceAll("/", "$")
+      .replaceAll("+", "$")
+      .replaceAll("-", "$")
+      .replaceAll("%", "$")
+      .split("$");
+    //
+    const numIndex = sign.length;
+    for (i = 0; i < numIndex; i++) {
+      //---> Percentage Calculate
+      if (sign.includes("%")) {
+        var index = sign.indexOf("%"); // place of sign
+        var firstNum = Number(numbers[index]); // previous number of sign
+        var secondNum = Number(numbers[index + 1]); // next number of sign
+        var result = parseInt(firstNum * 0.01 * secondNum); // calculate
+        delete numbers[index]; // deleted place to put the result and firstNum is deleted in array
+        numbers.splice(index + 1, 1); // secondNum is deleted
+        numbers[index] = result; // assign the result to deleted place
+        sign.splice(index, 1); // delete the sign from array for the operation to be correct.
+      } else if (sign.includes("*")) {
+        var index = sign.indexOf("*");
+        var firstNum = Number(numbers[index]);
+        var secondNum = Number(numbers[index + 1]);
+        var result = firstNum * secondNum;
+        delete numbers[index];
+        numbers.splice(index + 1, 1);
+        numbers[index] = result;
+        sign.splice(index, 1);
+      } else if (sign.includes("/")) {
+        var index = sign.indexOf("/");
+        var firstNum = Number(numbers[index]);
+        var secondNum = Number(numbers[index + 1]);
+        var result = parseInt(firstNum / secondNum);
+        delete numbers[index];
+        numbers.splice(index + 1, 1);
+        numbers[index] = result;
+        sign.splice(index, 1);
+      } else if (sign.includes("+")) {
+        var index = sign.indexOf("+");
+        var firstNum = Number(numbers[index]);
+        var secondNum = Number(numbers[index + 1]);
+        var result = firstNum + secondNum;
+        delete numbers[index];
+        numbers.splice(index + 1, 1);
+        numbers[index] = result;
+        sign.splice(index, 1);
+      } else if (sign.includes("-")) {
+        var index = sign.indexOf("-");
+        var firstNum = Number(numbers[index]);
+        var secondNum = Number(numbers[index + 1]);
+        var result = firstNum - secondNum;
+        delete numbers[index];
+        numbers.splice(index + 1, 1);
+        numbers[index] = result;
+        sign.splice(index, 1);
+      }
+    }
+    return numbers[0];
+  };
+  //---> calculate fonc. for calculation
+  const calculate = function (e) {
     if (
       operation.value === "0" ||
       operation.value === " " ||
@@ -86,8 +166,9 @@ $(document).ready(function () {
         button: "I got it ;))",
       });
     } else {
+      var value = operation.value;
       historyText.value = operation.value;
-      operation.value = eval(operation.value);
+      operation.value = basicCalculate(value);
       if (calcArray.length <= 2 && resultArray.length <= 2) {
         calcArray.push(historyText.value);
         resultArray.push(operation.value);
@@ -98,6 +179,23 @@ $(document).ready(function () {
         resultArray.push(operation.value);
       }
     }
+  };
+  // ---> Invert values of input to numbers and calculate
+  $("#equal").click(() => calculate());
+  // ---> Keyboard Calculation
+  $("#operation").keypress(function (e) {
+    e.keyCode === 13 && calculate();
+  });
+  // Checking number and special char in input
+  $("#operation").keypress(function (e) {
+    if (e.keyCode >= 65 || e.keyCode <= 90) {
+      swal({
+        title: "Hey Man!",
+        text: "Don't write text",
+        icon: "warning",
+        button: "Oh,sorry!",
+      });
+    }
   });
   // ---> History of calculation
   $(".screen---modes__history-icon .history-icon").click(function () {
@@ -106,33 +204,4 @@ $(document).ready(function () {
     secondCalc.innerHTML = calcArray[1] + "  -->  " + resultArray[1];
     thirdCalc.innerHTML = calcArray[2] + "  -->  " + resultArray[2];
   });
-  // ---> Keyboard Calculation
-  $("#operation").keypress(function(e){
-    if (e.keyCode === 13){
-      if (
-        operation.value === "0" ||
-        operation.value === " " ||
-        operation.value === null
-      ) {
-        swal({
-          title: "Stop!",
-          text: "Please,enter the values for evalution",
-          icon: "error",
-          button: "I got it ;))",
-        });
-      } else {
-        historyText.value = operation.value;
-        operation.value = eval(operation.value);
-        if (calcArray.length <= 2 && resultArray.length <= 2) {
-          calcArray.push(historyText.value);
-          resultArray.push(operation.value);
-        } else {
-          calcArray.shift();
-          calcArray.push(historyText.value);
-          resultArray.shift();
-          resultArray.push(operation.value);
-        }
-      }
-    }
-  })
 });
